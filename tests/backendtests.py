a76a51cb7c8563c3caace405db6b3bbe8e739377
DavidKwesi <100136822+DavidKwesi@users.noreply.github.com>
2024-12-12 07:10:19 +0000

    Create backendtests.py

---
+++ tests/backendtests.py
@@ -0,0 +1,8 @@
+import requests
+
+BASE_URL = "https://landslide-backend-7ipegonoia-uc.a.run.app/"
+
+def test_home_endpoint():
+    response = requests.get(BASE_URL)
+    assert response.status_code == 200
+
